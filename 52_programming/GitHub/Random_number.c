82a20e39b4fdf8ba9ca2c820e989d46f5ece3882
Tanjumul <crackerroot4@gmail.com>
2021-09-19 17:58:57 +0600

    commiting

--- 52_programming/GitHub/Random_number.c
+++ 52_programming/GitHub/Random_number.c
@@ -2,12 +2,12 @@
 #include<conio.h>
 #include<stdlib.h>
 int main() {
-int i , n , m ,number, rand(m) ;
+    int i , n , m ,number, rand(m) ;
 
 printf("How many random numbers you want:\n ");
-scanf("%d\n\n",&n);
+scanf("%d",&n);
 printf("Max number that you want to watch in random number counting:\n");
-scanf("%d\n\n",&m);
+scanf("%d",&m);
 printf("You want to see %d numbers that randomly shows you from 0 to %d numbers here\n",n,m);
 
 for(i=1 ; i<=n ; i++) //creating a loop to match with the desire numbers
